9139c26f9a29d124a2d69e99be389a9316ce6dd9
Alessio <alessio.peternelli@118118money.com>
2020-11-19 14:24:31 +0000

    First Commit!

---
+++ Web1/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Web1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ValuesController(IHttpClientFactory httpClientFactory)
+        {
+            if (httpClientFactory != null) _httpClientFactory = httpClientFactory;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            string[] values;
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                var response = await client.GetAsync("http://localhost:8277/api/values");
+                response.EnsureSuccessStatusCode();
+                values = JsonConvert.DeserializeObject<string[]>(await response.Content.ReadAsStringAsync());
+            }
+
+            return values;
+        }
+    }
+}
